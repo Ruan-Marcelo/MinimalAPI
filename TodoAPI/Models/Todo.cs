406460c81fc37edc1bbe1822023e37ee11de1350
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-05-06 21:35:42 -0300

    Model Todo

--- TodoAPI/Models/Todo.cs
+++ TodoAPI/Models/Todo.cs
@@ -2,5 +2,15 @@
 {
     public class Todo
     {
+        // Chave primária da tabela.
+        // O Entity Framework Core reconhecerá esta propriedade como o identificador do registro.
+        public int Id { get; set; }
+        // Nome ou descrição curta da tarefa.
+        // É inicializada com string vazia para evitar valores nulos no uso da aplicação.
+        public string Name { get; set; } = string.Empty;
+        // Indica se a tarefa foi concluída.
+        // false = pendente
+        // true = concluída
+        public bool IsComplete { get; set; }
     }
 }
