014780a42f8bcabda8e2e4fa366a70869e3d0dc6
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-05-06 21:51:08 -0300

    Migrations

--- TodoAPI/Data/TodoDb.cs
+++ TodoAPI/Data/TodoDb.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TodoAPI.Models;
 namespace TodoAPI.Data;
+
 // Classe de contexto do Entity Framework Core.
 // Ela faz a ponte entre a aplicação e o banco de dados.
 // É por meio dela que consultamos, inserimos, atualizamos e removemos dados.
@@ -8,11 +9,12 @@ public class TodoDb : DbContext
 {
     // Construtor que recebe as opções de configuração do contexto,
     // como a connection string e o provedor do banco de dados (SQL Server).
-    public TodoDb(DbContextOptions<Todo> options) : base(options)
-{
-}
-// Representa a tabela &quot;Todos&quot; no banco de dados.
-// O tipo DbSet&lt;Todo&gt; permite executar operações de consulta e persistência sobre a entidade Todo.
- public DbSet<Todo> Todos => Set<Todo>();
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+    {
+    }
 
+    // Representa a tabela "Todos" no banco de dados.
+    // O tipo DbSet<Todo> permite executar operações de consulta e persistência sobre a entidade Todo.
+    public DbSet<Todo> Todos => Set<Todo>();
 }
+
