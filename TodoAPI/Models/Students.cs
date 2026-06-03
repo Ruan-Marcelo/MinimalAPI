428aba0d7c64452723c4da29b614d07aab9d3e2a
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-06-03 20:01:13 -0300

    Campos para models

--- TodoAPI/Models/Students.cs
+++ TodoAPI/Models/Students.cs
@@ -1,8 +1,19 @@
-﻿namespace TodoAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace TodoAPI.Models
 {
     public class Students
     {
-        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O nome do aluno é obrigatório.")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage ="O email do aluno é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Informe um endereço de e-mail válido.")]
+        public String Email { get; set; }
+        public List<Todo> Todos { get; set; } = new();
 
 
 
